e4d7736076de653e86e80cfb17e85c3fda6e8a7a
hyloong <730018817@qq.com>
2016-11-04 10:30:52 +0800

    add snippets

--- init.el
+++ init.el
@@ -263,7 +263,11 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
-  ;; (setq default-directory "/media/sf_erl_work")
+  (if
+      (string= system-type "darwin")
+      (setq default-directory "~/Documents/dev")
+    (setq default-directory "/media/sf_erl_work")
+    )
   (setq projectile-enable-caching t)
   (setq-default cursor-type 'bar) 
   (delete-selection-mode t)
